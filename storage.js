4e1bfa18a41f3378662a55a72bf5a32e382992fa
Dragan Milicevic <dmilicevic@gmx.de>
2025-05-09 15:56:01 +0200

    Add explanatory comment for localStorage key

--- storage.js
+++ storage.js
@@ -3,6 +3,12 @@ storage.js
 Kapselt den Zugriff auf localStorage
 */
 
+/*
+  Wir nutzen den Key 'taskbuddy_tasks', um die Daten eindeutig
+  im localStorage abzulegen und eventuellen Namenskonflikten 
+  mit anderen Apps vorzubeugen.
+*/
+
 const storage = {
   save(tasks) {
     localStorage.setItem('taskbuddy_tasks', JSON.stringify(tasks));
